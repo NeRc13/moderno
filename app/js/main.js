d7ba363855b3d5afe8e5b0cbec1bcbf6efc83a1d
NeRc13 <myr26myr@yandex.ru>
2021-03-17 16:03:08 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
-$(function(){
+$(function () {
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        ratedFill: "#ffa726",
+        readOnly: true
+    });
+
+    $('.product__slider-body').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false
+    });
+
+    var mixer = mixitup('.product__body-box');
 
 
 });
